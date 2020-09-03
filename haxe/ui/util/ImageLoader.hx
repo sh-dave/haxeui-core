cb5a7183c017ad31d453e1d7d3db06628bc61b02
David Klein <dave@smokinheads.de>
2020-09-03 23:16:55 +0200

    ImageLoader: allow sys.* access from nodejs

--- haxe/ui/util/ImageLoader.hx
+++ haxe/ui/util/ImageLoader.hx
@@ -6,11 +6,11 @@ import haxe.ui.backend.ImageData;
 
 class ImageLoader {
     private var _resource:Variant;
-    
+
     public function new(resource:Variant) {
         _resource = resource;
     }
-    
+
     public function load(callback:ImageInfo->Void) {
         if (_resource.isString) {
             var stringResource:String = _resource;
@@ -29,19 +29,19 @@ class ImageLoader {
             }
         }
     }
-    
+
     private function loadFromHttp(url:String, callback:ImageInfo->Void) {
         #if js // cant use haxe.Http because we need responseType
-        
+
         var request = new js.html.XMLHttpRequest();
         request.open("GET", url);
         request.responseType = js.html.XMLHttpRequestResponseType.ARRAYBUFFER;
-        
+
         request.onreadystatechange = function(_) {
             if (request.readyState != 4) {
                 return;
             }
-            
+
             var s = try request.status catch (e:Dynamic) null;
             #if (haxe_ver >= 4)
             if (s == js.Syntax.code("undefined")) {
@@ -52,25 +52,25 @@ class ImageLoader {
                 s = null;
             }
             #end
-            
+
             if (s != null && s >= 200 && s < 400) {
                 Toolkit.assets.imageFromBytes(Bytes.ofData(request.response), callback);
             } else if (s == null) {
                 callback(null);
             } else {
                 #if debug
-                
+
                 var error:String = "Http Error #" + request.status;
                 switch(s) {
                     case 12029:
                         error = "Failed to connect to host";
-                    case 12007:    
+                    case 12007:
                         error = "Unknown host";
                     default:
                 }
-                
+
                 trace(error);
-                
+
                 #end
                 if (s == 0) { // Seems status = 0 is a CORS error, lets try and use a "normal" http request from the browser rather than XMLHttpRequest
                     Toolkit.assets.getImage(url, callback);
@@ -81,11 +81,11 @@ class ImageLoader {
         }
         request.onerror = function(x) {
         }
-        
+
         request.send();
-        
+
         #elseif cs // hxcs bytes are wrong in haxe.Http
-        
+
         var request = cs.system.net.WebRequest.Create(url);
         var buffer = new cs.NativeArray<cs.types.UInt8>(32768);
         var reader = new cs.system.io.StreamReader(request.GetResponse().GetResponseStream());
@@ -96,9 +96,9 @@ class ImageLoader {
         }
         reader.Close();
         Toolkit.assets.imageFromBytes(Bytes.ofData(memStream.ToArray()), callback);
-        
+
         #else
-        
+
         var http:haxe.Http = new haxe.Http(url);
         http.onData = function(data:String) {
             Toolkit.assets.imageFromBytes(Bytes.ofString(data), callback);
@@ -108,16 +108,16 @@ class ImageLoader {
             callback(null);
         }
         http.request();
-        
+
         #end
     }
 
     private function loadFromFile(filename, callback:ImageInfo->Void) {
-        #if sys
+        #if (sys || nodejs)
             if (sys.FileSystem.exists(filename) == false) {
                 callback(null);
             }
-            
+
             try {
                 Toolkit.assets.imageFromBytes(sys.io.File.getBytes(filename), callback);
             } catch (e:Dynamic) {
